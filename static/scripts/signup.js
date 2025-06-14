35c5d574d7ea0b2f3e2e196723550c14b73f3428
coderxrt <coderxrt@gmail.com>
2025-06-14 12:54:14 +0100

    Updated Js

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,10 +1,10 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const username = document.getElementById("username").value.trim()
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-if(username && email && password ){
+document.getElementById('submitBtn').addEventListener('click', ()=> {
+    const username = document.getElementById("username").value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    alert("js active")
+    if(username && email && password ){
+        alert("fetch actice")
     fetch("/new-user",{
         method:POST,
         headers:{
@@ -28,5 +28,8 @@ if(username && email && password ){
     .catch((error)=>{
         alert("Error"+ " " * error)
     })
-    }}
-
+    }
+    }else{
+        alert("One or more fields have not benn filled.")
+    }
+}
