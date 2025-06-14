6bcd54473d2a2e737a8b0bc2ba5fd4209d79419f
coderxrt <coderxrt@gmail.com>
2025-06-14 13:23:02 +0100

    Re instated Signup.js enitirely

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,35 +1,41 @@
-document.getElementById('submitBtn').addEventListener('click', ()=> {
-    const username = document.getElementById("username").value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    alert("js active")
-    if(username && email && password ){
-        alert("fetch actice")
+//
+
+//ByteVit
+
+document.getElementById("loginForm").addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const username = document.getElementById("username").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+  
+  if(username && email && password){
     fetch("/new-user",{
-        method:POST,
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            username:username,
-            email:email,
-            password:password
-        })
-    )
-    .then((response) => response.json())
-    .then((data)=>{
-        if(data.error){
-            let message = document.getElementById("message").innerText = data.error;
-            message.style.color ="red";
-        }else{
-            document.getElementById("message").innerText=data.success;
-        }
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        username : username,
+        email : email,
+        password : password
+      })
     })
-    .catch((error)=>{
-        alert("Error"+ " " * error)
+    .then((res => res.json()))
+    .then((data => {
+      let messageDiv = document.getElementById("message");
+      if(data.error){
+        messageDiv.innerText = data.error;
+        messageDiv.backgroundColor = "red"
+      }else{
+        messageDiv.innerText = data.success;
+        messageDiv.backgroundColor = "green";
+      }
+    }))
+    .catch((error) => {
+      console.error('Error:', error);
     })
-    }
-    }else{
-        alert("One or more fields have not benn filled.")
-    }
-}
+  }else{
+    alert("One or more fields have not been filled.")
+  }
+})
+
