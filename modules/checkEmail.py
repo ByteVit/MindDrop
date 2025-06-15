7ee76c050cb4bcee377cc783d3fee04178249f45
coderxrt <coderxrt@gmail.com>
2025-06-15 07:47:17 +0100

    Added Checkemail functionality regression pattern

---
+++ modules/checkEmail.py
@@ -0,0 +1,8 @@
+import re
+
+def check_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.fullmatch(pattern, email):
+        return True
+    else:
+        return False
