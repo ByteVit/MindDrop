4bd9d3d86e01cae6472a569c7415779032cb88f3
coderxrt <coderxrt@gmail.com>
2025-06-18 22:49:29 +0100

    Added new models for like and Followers in the database

--- models/Models.py
+++ models/Models.py
@@ -26,6 +26,8 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True,nullable=False)
 
     email = db.Column(db.String(120), unique=True,nullable=False)
+    likes = db.relationship('Likes',backref='owner', lazy=True)
+    followers = db.relationship('Follower', backref='user', lazy=True)
 
     password = db.Column(db.String(120),nullable=False)
     
@@ -41,10 +43,19 @@ class Quotes(db.Model):
     quote = db.Column(db.Text,nullable=False)
 
     category = db.Column(db.String(130),nullable=False,default="No Category")
-
+asi
     created_at = db.Column(db.Date,default=datetime.utcnow)
 
     unique_id = db.Column(db.ForeignKey("user.id"))
 
+class Follower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    unique_id = db.Column(db.ForeignKey('user.id'))
+
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    unique_id = db.Column(db.ForeignKey('user.id'))
 
 #More models to be added soon....
